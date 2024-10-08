c272c4110b256b6c13d306e0dc18a28893ea3676
chenly <mengkdilufei@gmail.com>
2024-10-08 21:53:28 +0800

    i18n

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n: {
+    defaultLocale: 'zh',
+    locales: ['en', 'zh'],
+  },
   images: {
     domains: ["localhost"],
     remotePatterns: [
